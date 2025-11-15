6853e772a02d1c152bce6be06e8dea133aefa5f8
Andreas Stenlund <a.stenlund@gmail.com>
2025-11-15 15:46:18 +0100

    Multiple Feeds (3) - Update cli

---
+++ FeatherPod.Cli/UserSettings.cs
@@ -0,0 +1,63 @@
+using System.Configuration;
+
+namespace FeatherPod.Cli;
+
+/// <summary>
+/// User settings stored in AppData/Local/FeatherPod.Cli/user.config
+/// </summary>
+public sealed class UserSettings : ApplicationSettingsBase
+{
+    public static UserSettings Default { get; } = (UserSettings)Synchronized(new UserSettings());
+
+    [UserScopedSetting]
+    [DefaultSettingValue("")]
+    public string LastUsedFeedDev
+    {
+        get => (string)this[nameof(LastUsedFeedDev)];
+        set => this[nameof(LastUsedFeedDev)] = value;
+    }
+
+    [UserScopedSetting]
+    [DefaultSettingValue("")]
+    public string LastUsedFeedTest
+    {
+        get => (string)this[nameof(LastUsedFeedTest)];
+        set => this[nameof(LastUsedFeedTest)] = value;
+    }
+
+    [UserScopedSetting]
+    [DefaultSettingValue("")]
+    public string LastUsedFeedProd
+    {
+        get => (string)this[nameof(LastUsedFeedProd)];
+        set => this[nameof(LastUsedFeedProd)] = value;
+    }
+
+    public string GetLastUsedFeed(string environment)
+    {
+        return environment switch
+        {
+            "Dev" => LastUsedFeedDev,
+            "Test" => LastUsedFeedTest,
+            "Prod" => LastUsedFeedProd,
+            _ => string.Empty
+        };
+    }
+
+    public void SetLastUsedFeed(string environment, string feedId)
+    {
+        switch (environment)
+        {
+            case "Dev":
+                LastUsedFeedDev = feedId;
+                break;
+            case "Test":
+                LastUsedFeedTest = feedId;
+                break;
+            case "Prod":
+                LastUsedFeedProd = feedId;
+                break;
+        }
+        Save();
+    }
+}
